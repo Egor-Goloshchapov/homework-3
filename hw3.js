a542b66d1048d8c92ee84e9f66467d7c34cfa29f
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-08-03 12:07:02 +0300

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,27 @@
+const result = 5 + 5 + "5";
+console.log(result);
+
+
+
+let email = "egorans.070710@gmail.com";
+if (email.includes('@')) {
+    console.log('Email contains the @ symbol');
+} else {
+    console.log('Email does NOT contain the @ symbol');//#endregion
+}
+let totalCharacters = email.length;
+console.log(`The total number of characters in the email: ${totalCharacters}`);
+
+
+
+const me = "My";
+const name = "name";
+const is = "is";
+const information = `${me} ${name} ${is} Viktor`;
+console.log(information);
+
+
+
+const userName = "Egor";
+const payment = "300 гривень";
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
