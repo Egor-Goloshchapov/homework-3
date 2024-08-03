139ac5a0653e550ac20d11808a9b19fa2c42eb41
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-08-03 14:52:09 +0300

    did hw-3

--- js/hw3.js
+++ js/hw3.js
@@ -7,7 +7,7 @@ let email = "egorans.070710@gmail.com";
 if (email.includes('@')) {
     console.log('Email contains the @ symbol');
 } else {
-    console.log('Email does NOT contain the @ symbol');//#endregion
+    console.log('Email does NOT contain the @ symbol');
 }
 let totalCharacters = email.length;
 console.log(`The total number of characters in the email: ${totalCharacters}`);
